6dbcbf2361622dec02eebafdc9f8eaa344f02d6c
Walter Huf <hufman@gmail.com>
2019-03-03 19:49:33 -0800

    Adds a method to try probing for a car connection
    In case the app crashed, we can try probing for the car connection
    and trying to reconnect to it. However, the initial connection
    announcement contains some necessary information, such as brand and
    instance_id, not sure yet how to figure that information out..
    If we guess wrong, the client app will crash because of using the
    incorrect brand's cert

--- src/main/java/me/hufman/idriveconnectionkit/android/IDriveConnectionListener.kt
+++ src/main/java/me/hufman/idriveconnectionkit/android/IDriveConnectionListener.kt
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import java.io.IOException
+import java.net.Socket
 
 class IDriveConnectionListener : BroadcastReceiver() {
 	private val TAG = "IDriveConnectionListen"
@@ -30,8 +32,18 @@ class IDriveConnectionListener : BroadcastReceiver() {
 			private set
 		var callback: Runnable? = null
 
+		private var connectThread: Thread? = null
 		fun reset() {
 			isConnected = false
+		}
+
+		fun tryConnect() {
+			synchronized(IDriveConnectionListener::class.java) {
+				if (!isConnected && connectThread?.isAlive != true) {
+					connectThread = ConnectThread()
+					connectThread?.start()
+				}
+			}
 		}
 	}
 
@@ -52,6 +64,27 @@ class IDriveConnectionListener : BroadcastReceiver() {
 		if (intent.action == "com.bmwgroup.connected.accessory.ACTION_CAR_ACCESSORY_DETACHED") {
 			isConnected = false
 			if (callback != null) callback?.run()
+		}
+	}
+
+	private class ConnectThread: Thread() {
+		override fun run() {
+			val PORTS = listOf(4004, 4005, 4006, 4007, 4008)
+			for (port in PORTS) {
+				try {
+					val socket = Socket("127.0.0.1", port)
+					if (socket.isConnected) {
+						// we found a car proxy! probably
+						IDriveConnectionListener.isConnected = true
+						IDriveConnectionListener.brand = "bmw"  // a complete guess! but will crash if it's incorrect
+						IDriveConnectionListener.host = "127.0.0.1"
+						IDriveConnectionListener.port = port
+						IDriveConnectionListener.callback?.run()
+					}
+				} catch (e: IOException) {
+					// this port isn't open
+				}
+			}
 		}
 	}
 }
